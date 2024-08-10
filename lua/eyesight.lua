d99084a60c8b41cd32875d2f418f0da68dadcf85
filip <filip125@icloud.com>
2024-08-10 17:41:20 +0200

    basics work

--- lua/eyesight.lua
+++ lua/eyesight.lua
@@ -4,14 +4,24 @@ local function remind()
 	require("notify")("Loook at something 6m away for 20 seconds")
 end
 
-local time = 0
+Eyesight_Time = 0
 
 local function set_time()
-	time = os.time()
+	Eyesight_Time = os.time()
+	require("notify")("Starting the 20 minut eyesight timer")
+end
+
+local function check_time()
+	local currentime = os.time()
+	if currentime - Eyesight_Time > 20 * 60 then
+		remind()
+		Eyesight_Time = currentime
+	end
 end
 
 local function setup()
-	vim.api.nvim_create_autocmd("VimEnter", { augroup = augroup, callback = set_time, once = true })
+	vim.api.nvim_create_autocmd("VimEnter", { group = augroup, callback = set_time, once = true })
+	vim.api.nvim_create_autocmd("CursorMoved", { group = augroup, callback = check_time })
 end
 
 return { setup = setup }
